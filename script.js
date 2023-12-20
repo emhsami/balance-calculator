79e4c7dc5a15ccbc309a007f8de248c77d9b1f2c
Sami <emhsami@gmail.com>
2023-12-20 15:37:38 +0600

    structure and js connection done

---
+++ script.js
@@ -0,0 +1,5 @@
+var availableBalance = document.querySelector("#available-balance");
+var inputBalance = document.querySelector("#input-balance");
+var depoBtn = document.querySelector("#depo-btn");
+var expBtn = document.querySelector("#exp-btn");
+var resetBtn = document.querySelector("#reset-btn")
