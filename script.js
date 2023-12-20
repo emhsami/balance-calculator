630bf543c53cd7cf26e25ceb6070b5724d9d5acb
Sami <emhsami@gmail.com>
2023-12-20 18:13:17 +0600

    deposit action done

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var availableBalance = document.querySelector("#available-balance");
-var inputBalance = document.querySelector("#input-balance");
+var inputAmount = document.querySelector("#input-amount")
 var depoBtn = document.querySelector("#depo-btn");
 var expBtn = document.querySelector("#exp-btn");
-var resetBtn = document.querySelector("#reset-btn")
+var resetBtn = document.querySelector("#reset-btn");
+
+var totalAmount = availableBalance.innerHTML = 50000;
+
+// var fixedAmount = (parseInt(totalAmount));
+
+depoBtn.addEventListener("click", (e)=>{
+    // e.preventDefault();
+    var converted = inputAmount.value;
+    var converting = parseInt(converted);
+    availableBalance.innerHTML = totalAmount + converting;
+})
