95ca331cf599bdff36cfadb91b3458168dccd739
Sami <emhsami@gmail.com>
2023-12-21 18:49:45 +0600

    calculation done

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 var availableBalance = document.querySelector("#available-balance");
-var inputAmount = document.querySelector("#input-amount")
-var depoBtn = document.querySelector("#depo-btn");
-var expBtn = document.querySelector("#exp-btn");
-var resetBtn = document.querySelector("#reset-btn");
+var inputAmount = document.querySelector("#input-amount");
+var transList = document.querySelector("#trans-list");
 
-var totalAmount = availableBalance.innerHTML = 50000;
+var totalAmount = parseInt(availableBalance.innerHTML);
 
-// var fixedAmount = (parseInt(totalAmount));
-
-depoBtn.addEventListener("click", (e)=>{
-    // e.preventDefault();
+function depoBtn() {
     var converted = inputAmount.value;
     var converting = parseInt(converted);
-    availableBalance.innerHTML = totalAmount + converting;
-})
+    var result = totalAmount + converting;
+
+    if (converted.trim() !== "") {
+        updateTransaction("Deposit", converting);
+        totalAmount = result;
+        updateAvailableBalance(totalAmount);
+    }
+
+    inputAmount.value = "";
+}
+
+function expBtn() {
+    var converted = inputAmount.value;
+    var converting = parseInt(converted);
+    var result = totalAmount - converting;
+
+    if (converted.trim() !== "") {
+        updateTransaction("Expense", converting);
+        totalAmount = result;
+        updateAvailableBalance(totalAmount);
+    }
+
+    inputAmount.value = "";
+}
+
+function updateTransaction(type, amount) {
+    var listItem = document.createElement("li");
+    listItem.textContent = `${type}: ${amount}`;
+    listItem.classList.add(type.toLowerCase());
+    transList.insertBefore(listItem, transList.firstChild);
+}
+
+function updateAvailableBalance(amount) {
+    availableBalance.innerHTML = amount;
+}
